07feaa8cb80644fa84695fd5a575e37ce91e3b1b
Oliver Stump <137001490+xlly-s@users.noreply.github.com>
2026-05-29 13:01:54 +0100

    Added main python, and img files.
    Added the main code and the image files to go with it, some are redundant at this time...

---
+++ main.py
@@ -0,0 +1,100 @@
+import time
+import pyautogui
+import keyboard
+
+SEARCHREGION = (537, 294, 1376, 387)
+NOAUCTIONSREGION = (977,168,1825,944)
+SOLDREGION = (119, 212, 263, 271)
+AVAILABLEREGION = (898,617,1458,888)
+
+CONFIDENCE = 0.82
+CHECK_DELAY = 0.15
+
+running = True
+
+
+
+def stop():
+    global running
+    running = False
+    print("Stopped.")
+
+keyboard.add_hotkey("ctrl+c", stop)
+
+
+
+
+def exists(img, region=None):
+    return pyautogui.locateOnScreen(
+        img,
+        confidence=CONFIDENCE,
+        region=region
+    )
+
+
+print("Starting in 3 seconds...")
+time.sleep(3)
+
+
+
+while running:
+    searchbox = pyautogui.locateOnScreen(
+        "imgs/searchmenu.png",
+        confidence=0.8,
+        region=SEARCHREGION,
+        grayscale=True
+    )
+
+    if searchbox:
+        print("Search menu opened:", searchbox)
+        pyautogui.press("enter")
+        time.sleep(0.5)
+            
+        noauctions = pyautogui.locateOnScreen(
+            "imgs/noauctions.png",
+            confidence=0.3,
+            region=NOAUCTIONSREGION,
+            grayscale=True
+        )
+        available = pyautogui.locateOnScreen(
+            "imgs/open.png",
+            confidence=0.4,
+            region=AVAILABLEREGION,
+            grayscale=True
+        )
+        
+
+        time.sleep(0.5)
+        if noauctions:
+            print("No cars:",noauctions)
+            pyautogui.press("esc")
+            time.sleep(1)
+            pyautogui.press("enter")
+            continue
+        elif available:
+            pyautogui.press("y")
+            time.sleep(0.1)
+            pyautogui.press("down")
+            time.sleep(0.1)
+            pyautogui.press("enter")
+            time.sleep(0.1)
+            pyautogui.press("enter")
+            time.sleep(4)
+            pyautogui.press("enter")
+            time.sleep(0.2)
+            pyautogui.press("esc")
+            time.sleep(0.2)
+            pyautogui.press("esc")
+            time.sleep(1)
+            pyautogui.press("enter")
+        else:
+            print("unknown, returning")
+            pyautogui.press("esc")
+            time.sleep(1)
+            pyautogui.press("enter")
+
+        
+    else:
+        print("Not found")
+
+
